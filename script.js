6744cefe26d1617059544ae3ed735c690ea72eb1
Yatrikoxi <tomas_novak@centrum.cz>
2023-06-07 08:27:13 +0200

    start js build

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let bookAuthor = document.getElementById("bookAuthor");
 let bookPages = document.getElementById("bookPages");
 let readOrNot = document.getElementById("readOrNot");
 let yesBtn = document.getElementsByClassName("yesBtn");
+let form = document.getElementById("form");
+let submitBtn = document.getElementById("submitBtn");
+let closeSymbol = document.getElementById("closeBtn");
 
 function openPopup() {
     popupForm.classList.add("openPopup")
@@ -20,14 +23,31 @@ closeSymbol.addEventListener('click', () => {
 })
 
 // Library constructor
-let bName = "";
-let bAuthor = "";
-let bPages = "";
-let yesOrNot = false;
 
-function Book(name, author, pages,Boolean) {
+const formBook = function Book(name, author, pages,Boolean) {
     this.name = name,
     this.author = author,
     this.pages = pages,
-    this.Boolean = Boolean
+    this.Boolean = Boolean;
 }
+const addBookToLibrary = () => {
+    let title = bookName.value;
+    let author = bookAuthor.value;
+    let pages = bookPages.value;
+    let read = getReadValue();
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook)
+}
+
+const getReadValue = () => {
+    if(form.querySelector('input[id="readOrNot"]:checked').value == 'yes') return true;
+    else return false;
+  }
+
+const myLibrary = [];
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+    console.log(myLibrary)
+})

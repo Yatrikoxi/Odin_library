cd9213e14fb17b2195a50ea15255b9a45e4f0df4
Yatrikoxi <tomas_novak@centrum.cz>
2023-06-08 16:55:36 +0200

    card create logic - start

--- script.js
+++ script.js
@@ -32,18 +32,18 @@ closeSymbol.addEventListener('click', () => {
 
 // Library constructor
 const myLibrary = [];
-const formBook = function Book(name, author, pages, boolean) {
+function Book(name, author, pages, boolean) {
     this.name = name,
     this.author = author,
     this.pages = pages,
     this.boolean = boolean;
 }
 const addBookToLibrary = () => {
-    let title = bookName.value;
+    let name = bookName.value;
     let author = bookAuthor.value;
     let pages = bookPages.value;
     let read = getReadValue();
-    let newBook = new formBook(title, author, pages, read);
+    let newBook = new Book(name, author, pages, read);
     myLibrary.push(newBook)
 }
 
@@ -51,15 +51,57 @@ const getReadValue = () => {
     if(readOrNot.value == "yes") return true;
     else return false;
 }
-function bookCard(myLibrary) {
+function bookCard() {
 let card = document.createElement("div");
 card.classList.add("bookCard");
 bottomWrapper.appendChild(card);
+let name = document.createElement("p");
+name.classList.add("headerText")
+name.innerHTML = 'Name: ';
+let nameContent = document.createElement("span");
+nameContent.classList.add("dataText");
+nameContent.innerHTML = `${this.title}`;
+let author = document.createElement("p");
+author.classList.add("headerText")
+author.innerHTML = 'Author: ';
+let authorContent = document.createElement("span");
+authorContent.classList.add("dataText");
+authorContent.innerHTML = `${this.author}`;
+let pages = document.createElement("p");
+pages.classList.add("headerText")
+pages.innerHTML = 'Pages: ';
+let pagesContent = document.createElement("span");
+pagesContent.classList.add("dataText");
+pagesContent.innerHTML = `${this.pages}`;
+
+let read = document.createElement("p");
+read.classList.add("headerText");
+read.innerHTML = 'Read: ';
+let readContent = document.createElement("button");
+ if (getReadValue() == true) {
+    readContent.classList.add("noBtn")
+    readContent.textContent = "YES";
+}else {
+    readContent.classList.add("yesBtn")
+    readContent.textContent = "NO";
+}
+// Content.classList.add("dataText");
+// authorContent.innerHTML = `${this.author}`;
+name.appendChild(nameContent);
+author.appendChild(authorContent);
+pages.appendChild(pagesContent);
+read.appendChild(readContent);
+
+card.appendChild(name);
+card.appendChild(author);
+card.appendChild(pages);
+card.appendChild(read);
 }
 
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
     addBookToLibrary();
+    bookCard(myLibrary)
     console.log(myLibrary);
     clearForm();
     closePopup();
